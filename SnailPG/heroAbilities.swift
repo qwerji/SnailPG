d6afcc05fdafde355ba0f86d4e6d277cc165959b
andycarver <andrewmcarver@gmail.com>
2017-02-03 11:19:36 -0800

    abilities functionality

--- SnailPG/heroAbilities.swift
+++ SnailPG/heroAbilities.swift
@@ -77,9 +77,53 @@ extension Hero {
         return (log, result)
     }
     //thief abilities
-    
-
+    func backStab(_ target: Monster) -> (String, String){
+        let spell = "Back Stab"
+        let stabDamage = Int(self.dexterity * 2)
+        target.damage -= stabDamage
+        self.mana -= 40
+        let result = "Spell Cast"
+        let log = "\(self.name) casts \(spell) and blocked \(stabDamage) damage from \(target.name)!"
+        return (log, result)
+    }
 }
+
+let AbilityList : [String:[String:Any]] = [
+    "Warrior": [
+        "Shield Bash" : [
+            "name" : "Shield Bash",
+            "cost" : 30,
+        ],
+        "Desperate Strike" : [
+            "name" : "Desperate Strike",
+            "cost" : 30,
+        ],
+    ],
+    "Mage": [
+        "Magic Missile" : [
+            "name" : "Magic Missile",
+            "cost" : 30,
+        ],
+        "Cure" : [
+            "name" : "Cure",
+            "cost" : 30,
+        ],
+        "Leech" : [
+            "name" : "Leech",
+            "cost" : 30,
+        ],
+        "Mana Shield" : [
+            "name" : "Mana Shield",
+            "cost" : 30,
+        ],
+    ],
+    "Thief": [
+        "Back Stab" : [
+            "name" : "Back Stab",
+            "cost" : 40,
+        ],
+    ]
+]
 
 
 
