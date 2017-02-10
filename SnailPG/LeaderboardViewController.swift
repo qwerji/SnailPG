dee82de75288a4d85cf8df533320805903b72163
qwerji <bentswanson@gmail.com>
2017-02-09 18:33:01 -0800

    usernames and leaderboard functionality added

--- SnailPG/LeaderboardViewController.swift
+++ SnailPG/LeaderboardViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
-import FirebaseAuth
+import Firebase
 
 class LeaderboardViewController: UIViewController {
+    
+    @IBOutlet weak var leaderboardTableView: UITableView!
+    
+    var leaderboard = [FIRDataSnapshot]()
 
     @IBAction func backButtonPressed(_ sender: UIButton) {
         if let navController = self.navigationController {
@@ -27,5 +31,41 @@ class LeaderboardViewController: UIViewController {
             print ("Error signing out: %@", signOutError)
         }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        leaderboardTableView.delegate = self
+        leaderboardTableView.dataSource = self
+        update()
+        ref.child("users").observe(.childChanged, with: {(snapshot) in
+            self.update()
+        })
+    }
+    
+    func update() {
+        let query = ref.child("users").queryOrdered(byChild: "totalVictories").queryLimited(toLast: 10)
+        query.observeSingleEvent(of: .value, with: { (snapshot) in
+            self.leaderboard = []
+            for data in snapshot.children.allObjects as! [FIRDataSnapshot] {
+                self.leaderboard.insert(data, at: 0)
+            }
+            self.leaderboardTableView.reloadData()
+        })
+    }
 
+}
+
+extension LeaderboardViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leaderboardCell", for: indexPath)
+        cell.textLabel?.text = "\(leaderboard[indexPath.row].childSnapshot(forPath: "name").value!)"
+        cell.detailTextLabel?.text = "Victories: \(leaderboard[indexPath.row].childSnapshot(forPath: "totalVictories").value!)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return leaderboard.count
+    }
+    
 }
--- SnailPG/LogInViewController.swift
+++ SnailPG/LogInViewController.swift
@@ -11,41 +11,91 @@ import FirebaseAuth
 
 class LogInViewController: UIViewController {
     
+    @IBOutlet weak var switchLoginRegisterButtonPressed: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var firebaseErrorTextField: UILabel!
     
+    var login = true
+    var created = false
+    var loggedInUser: FIRUser?
+    
+    @IBAction func switchLoginRegisterButtonPressed(_ sender: UIButton) {
+        if login {
+            login = false
+            usernameTextField.isHidden = false
+            switchLoginRegisterButtonPressed.setTitle("Already Have An Account?", for: .normal)
+            logInButton.setTitle("Register", for: .normal)
+        } else {
+            login = true
+            usernameTextField.isHidden = true
+            switchLoginRegisterButtonPressed.setTitle("Don't Have An Account?", for: .normal)
+            logInButton.setTitle("Log In", for: .normal)
+        }
+        firebaseErrorTextField.text = ""
+    }
+    
     @IBAction func logInButtonPressed(_ sender: UIButton) {
-        if let email = emailTextField.text, let password = passwordTextField.text {
-            FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
-                if let e = error {
-                    self.firebaseErrorTextField.text = e.localizedDescription
-                } else {
-                    self.firebaseErrorTextField.text = "Logged in!"
-                    if let navController = self.navigationController {
-                        navController.popViewController(animated: true)
+        if created {
+            self.setUsername()
+        } else if login {
+            
+            if let email = emailTextField.text, let password = passwordTextField.text {
+                FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+                    if let e = error {
+                        self.firebaseErrorTextField.text = e.localizedDescription
+                    } else {
+                        self.firebaseErrorTextField.text = "Logged in!"
+                        if let navController = self.navigationController {
+                            navController.popViewController(animated: true)
+                        }
                     }
                 }
             }
+            
+        } else {
+            
+            if let email = emailTextField.text, let password = passwordTextField.text {
+                FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+                    if let e = error {
+                        self.firebaseErrorTextField.text = e.localizedDescription
+                    } else {
+                        self.loggedInUser = user
+                        self.setUsername()
+                        self.created = true
+                        self.switchLoginRegisterButtonPressed.isHidden = true
+                        self.logInButton.setTitle("Set Username", for: .normal)
+                    }
+                }
+            }
+            
         }
+        
     }
     
-    @IBAction func registerButtonPressed(_ sender: UIButton) {
-        if let email = emailTextField.text, let password = passwordTextField.text {
-            FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
-                if let e = error {
-                    self.firebaseErrorTextField.text = e.localizedDescription
+    func setUsername() {
+        if let user = loggedInUser {
+            let request = user.profileChangeRequest()
+            request.displayName = usernameTextField.text!
+            request.commitChanges(completion: { error in
+                if let er = error {
+                    self.firebaseErrorTextField.text = er.localizedDescription
                 } else {
                     self.firebaseErrorTextField.text = "Registered and Logged in!"
                     
                     // Set achievements up
-                    ref.child("users").child((user?.uid)!).setValue(["totalVictories": 0])
+                    ref.child("users").child(user.uid).setValue([
+                        "totalVictories": 0,
+                        "name": user.displayName ?? ""
+                    ])
                     
                     if let navController = self.navigationController {
                         navController.popViewController(animated: true)
                     }
                 }
-            }
+            })
         }
     }
     
@@ -58,6 +108,14 @@ class LogInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         firebaseErrorTextField.text = ""
+        usernameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        usernameTextField.isHidden = true
     }
 
+}
+
+extension LogInViewController: UITextFieldDelegate {
+    
 }
